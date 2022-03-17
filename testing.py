452977725d11f7cc9608fcca8be02e2492a62411
Cristian Campos Agüero <ccampos04@gmail.com>
2022-03-17 14:12:35 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,13 +1,13 @@
 import Practica;
 import pytest;
-# from unittest import mock
-from unittest.mock import patch
+from unittest import mock
+# from unittest.mock import patch
 
 modulo = Practica
 
 def test_division_1():  
-    opcion = ''
-    valor = ''
+    opcion = '2'
+    valor = '15'
     with mock.patch('builtins.input', side_effect = [opcion, valor]):
         assert modulo.convertir() == -9.444444444444445
                     
